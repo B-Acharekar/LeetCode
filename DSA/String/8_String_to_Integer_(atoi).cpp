f5bd988d67112b4e90529719f71a05ad03ca31ca
B-Acharekar <153992410+B-Acharekar@users.noreply.github.com>
2025-05-28 22:21:47 +0530

    String: 8. String to Integer (atoi)

---
+++ DSA/String/8_String_to_Integer_(atoi).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.length();
+        while (i < n && s[i] == ' ') i++;
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+        long result = 0;
+        while (i < n && isdigit(s[i])) {
+            result = result * 10 + (s[i] - '0');
+            if (sign == 1 && result > INT_MAX) return INT_MAX;
+            if (sign == -1 && -result < INT_MIN) return INT_MIN;
+            i++;
+        }
+        return sign * result;
+    }
+};
+
+int main() {
+    Solution sol;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    int output = sol.myAtoi(input);
+    cout << "Converted integer: " << output << endl;
+    return 0;
+}
